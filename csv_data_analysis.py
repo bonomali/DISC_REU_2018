dbcb14c9cfed94491abcf5f68f1618ffd3268906
Maggie Goulden <gouldenm@tcd.ie>
2018-06-04 14:48:10 +0100

    Median vs Mean testing seems to work fine. Included a method to get all zero-click days added as well.

--- csv_data_analysis.py
+++ csv_data_analysis.py
@@ -21,7 +21,7 @@ with open(csv_filepath) as f:
 
 
 ##Maybe see how -all- entries vary over time?
-def plot_activity_by_date(data_dict, name = "N/A", color = 'black', divisor = 1, storage_array = np.array([])):
+def plot_activity_by_date(data_dict, name = "N/A", color = 'black', divisor = 1, total_days = 133):
 	entries_by_date = Counter(x["date"] for x in data_dict)
 	
 	dates_strings = entries_by_date.keys()
@@ -35,8 +35,24 @@ def plot_activity_by_date(data_dict, name = "N/A", color = 'black', divisor = 1,
 	np_dates = np.array(dates_sorted)
 	np_values = np.array(values)
 	
+	'''#If you want to include every single day, even those with zero clicks, use the following:
+	base = dt.date(2018, 1, 2)
+	all_dates = np.array([base + dt.timedelta(days=i) for i in xrange(total_days)])
+	
+	missing_dates = np.pad(np_dates, (0,total_days), 'constant', constant_values = (0, 0))
+	missing_values = np_values
+	
+	for i in range(133):
+		if all_dates[i] != missing_dates[i]:
+			missing_dates = np.insert(missing_dates, i, all_dates[i])
+			missing_values = np.insert(missing_values, i, 0)
+	missing_dates = missing_dates[:-total_days]
+	
+	np_dates = missing_dates
+	np_values = missing_values
+		
 	mean_val = np.mean(np_values)
-	median_val = np.median(np_values)
+	median_val = np.median(np_values)'''
 	
 	np_all_data = {"dates_array": np_dates, "values_array": np_values, "name": name}
 	
@@ -72,7 +88,7 @@ entries_by_name = Counter(x['account_name'] for x in chopped_data)
 names = entries_by_name.keys()
 col_counter = 0
 
-list_of_all_data = []
+all_data = []
 for name in names:
 	plt.figure()
 	indices = find(chopped_data, "account_name", name)
@@ -83,6 +99,10 @@ for name in names:
 	list_of_all_data.append(new_data)
 	
 	col_counter += 1
+
+
+
+
 
 
 '''divisor = float(len(Counter(x["account_name"] for x in chopped_data)))
