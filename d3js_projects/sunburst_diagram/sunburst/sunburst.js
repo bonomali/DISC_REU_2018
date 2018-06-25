a3a4dc663005f533980aff9939a21a2c70128edc
egronda <egronda@nd.edu>
2018-06-25 16:55:30 -0400

    Sunburst active sequence data

--- d3js_projects/sunburst_diagram/sunburst/sunburst.js
+++ d3js_projects/sunburst_diagram/sunburst/sunburst.js
@@ -1,5 +1,5 @@
 var width = 960,
-    height = 700,
+    height = 600,
     radius = (Math.min(width, height) / 2) - 10;
 
 var formatNumber = d3.format(",d");
@@ -20,8 +20,12 @@ var arc = d3.arc()
     .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
     .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
 
+var sequence = 	d3.select("#active_sequence").append("svg")
+	.attr("width" , 1000)
+	.attr("height" , 50)
 
-var svg = d3.select("body").append("svg")
+
+var svg = d3.select("#sunburst").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
@@ -36,8 +40,10 @@ d3.json("sunburst_data.json", function(error, root) {
       .data(partition(root).descendants())
     .enter().append("path")
       .attr("d", arc)
-      .style("fill", function(d) { return color((d.children ? d : d.parent).data.name); })
+      .style("fill", function(d) { return color(d.data.name); })
       .on("click", click)
+	  .on("mouseover" , mouseover)
+	  .on("mouseout" , mouseleave)
     .append("title")
       .text(function(d) { return d.data.name + "\n" + formatNumber(d.value); });
 });
@@ -56,11 +62,35 @@ function click(d) {
 }
 
 d3.select(self.frameElement).style("height", height + "px");// mouse over effects: highlight, display sequences
+
+// Mouse over effects
 function mouseover(d) {
-	console.log("Mouseover" + d.name);
+
+	// display on screen the current sequence
+	node = d;
+	while(node.parent){
+		// add rectangles
+		sequence.append("rect")
+			.attr("id" , "active_sequence_data")
+			.attr("width" , 100)
+			.attr("height" , 50)
+			.style("fill", function(e) { return color(node.data.name); })
+			.attr("transform" , function(e){ return "translate(" + (node.depth * 100 - 100) + ", 0)"})
+		// add text
+		sequence.append("text")
+			.attr("id" , "active_sequence_data")
+    		.attr("x", function(e) { return node.depth * 100 - 50; })
+    		.attr("y", 25)
+    		.attr("dy", ".35em")
+    		.text(function(e) { return node.data.name; });
+		
+		node = node.parent;
+	}
+	
 }
 
+// clear mouse over effects
 function mouseleave(d) {
-	console.log("mouseleave");
+	d3.selectAll("#active_sequence_data").remove();
 }
 
